c6ae56769f1f7035d3a4a766065e150a7dc2c96d
Thibault <thibault.castillon@epitech.eu>
2022-03-25 14:12:34 +0100

    [Edit] change attack skill

--- Actions/attack.rb
+++ Actions/attack.rb
@@ -1,20 +1,12 @@
 require_relative "../hero"
 require_relative "../Enemy/enemy.rb"
 
-def attack(hero, enemy)
-    puts `clear`
-    if enemy.size == 1
-        damage = (-1) * hero.getAtk() + enemy[0].getDef()
-        damage = (damage > 0) ? 0 : damage
-        enemy[0].changeHP(damage);
-        if (enemy[0].getHP() <= 0)
-            puts "#{enemy[0]} is dead !! No enemy left ! You WIN !!"
-            enemy.delete_at(0)
-            return enemy
-        else
-            puts "#{enemy[0]} takes #{hero.getAtk()} dmg. #{enemy[0]} has #{enemy[0].getHP} HP left."
-        end
-    end
-    sleep(4)
-    return enemy
+def attack(init, target)
+    damage = (init.getAtk() * -1) + target.getDef()
+    damage = (damage > 0) ? 0 : damage
+    target.changeHP(damage)
+
+    targetHP = (target.getHP() < 0) ? 0 : target.getHP()
+    puts "#{target} takes #{damage} and has now #{targetHP} HP left."
+    return target
 end
