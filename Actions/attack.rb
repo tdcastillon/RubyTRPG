a8f3ad06f6fddd7d92ce3f2965d2152de039b0a1
Thibault <thibault.castillon@epitech.eu>
2022-03-27 04:40:43 +0200

    [Edit] Attakc Skill - Modify Formula

--- Actions/attack.rb
+++ Actions/attack.rb
@@ -2,11 +2,10 @@ require_relative "../hero"
 require_relative "../Enemy/enemy.rb"
 
 def attack(init, target)
-    damage = (init.getAtk() * -1) + target.getDef()
+    damage = (init.getAtk * -2) + target.getDef
     damage = (damage > 0) ? 0 : damage
     target.changeHP(damage)
 
-    targetHP = (target.getHP() < 0) ? 0 : target.getHP()
+    targetHP = (target.getHP < 0) ? 0 : target.getHP
     puts "#{target} takes #{damage} and has now #{targetHP} HP left."
-    return target
 end
