8058568540e7a8b2c99cd39acf191b913f74933f
Thibault <thibault.castillon@epitech.eu>
2022-03-27 05:59:43 +0200

    [ADD] Skill Option in BattleMenu

--- Menu/BattleMenu.rb
+++ Menu/BattleMenu.rb
@@ -3,6 +3,7 @@ require_relative "../Enemy/enemy.rb"
 require_relative "../Actions/attack.rb"
 
 require_relative "EnemySelector.rb"
+require_relative "SkillMenu.rb"
 require_relative "../Screens/enemy.rb"
 
 def battle_menu(game)
@@ -26,7 +27,25 @@ def battle_menu(game)
             puts `clear`
             puts("\nBattle Menu\n\n1 Attack\n2 Skills\n3 Inventory\n4 Flee\nEnter to validate option")
         when 2
-            puts "2"
+            valid = SkillMenu(game.getHero, game.getHero.GetClass.getSkills, game.getEnemy)
+            if !valid
+                puts `clear`
+                puts("\nBattle Menu\n\n1 Attack\n2 Skills\n3 Inventory\n4 Flee\nEnter to validate option")
+            else
+                xp = check_life(game.getEnemy, game.getHero)
+                if xp != 0
+                    puts "#{game.getHero} has gained #{xp} xp!\n"
+                end
+                game.getHero.gainXp(xp)
+                if game.getEnemy.size == 0
+                    puts "You win !!\n"
+                    break
+                end
+                enemy_turn(game)
+                sleep(3)
+                puts `clear`
+                puts("\nBattle Menu\n\n1 Attack\n2 Skills\n3 Inventory\n4 Flee\nEnter to validate option")
+            end
         when 3
             puts "3"
         when 4
@@ -34,7 +53,7 @@ def battle_menu(game)
             break
         else
             puts `clear`
-            puts("\nBattle Menu\n\n1 Attack\n2 Inventory\n3 Flee\nEnter to validate option")
+            puts("\nBattle Menu\n\n1 Attack\n2 Skills\n3 Inventory\n4 Flee\nEnter to validate option")
             sleep(1)
         end
         user = gets.chomp.to_i 
