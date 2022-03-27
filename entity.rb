b573632530e508ae3a7906bc76381e1e3493dc72
Thibault <thibault.castillon@epitech.eu>
2022-03-27 10:13:06 +0200

    [ADD] MaxMp + MaxHP + LevelUp [EDIT] changeHP method

--- entity.rb
+++ entity.rb
@@ -7,8 +7,11 @@ class Entity
         @def = 1
         @mdf = 1
         @hp = 1
+        @max_hp = @hp
         @mp = 1
+        @max_mp = @mp
         @lv = 1
+        @xp_rate = 1
     end
     def getDef
         @def
@@ -31,17 +34,26 @@ class Entity
     def getMat
         @mat
     end
+    def getMaxMat
+        @max_mp
+    end
     def setMat(mat)
         @mat = mat
+        @max_mp = mat
     end
     def getHP
         @hp
     end
+    def getMaxHP
+        @max_hp
+    end
     def setHp(hp)
         @hp = hp
+        @max_hp = hp
     end
     def changeHP(value)
         @hp = @hp + value
+        @hp = (@hp > @max_hp) ? @max_mp : @hp
     end
     def getLevel
         @lv
@@ -51,6 +63,17 @@ class Entity
     end
     def getMp
         @mp
+    end
+    def setXprate(xp_rate)
+        @xp_rate = xp_rate
+    end
+    def LevelUp()
+        @atk = @atk * @xp_rate * @level
+        @def = @def * @xp_rate * @level
+        @mdf = @def * @xp_rate * @level
+        @mat = @mat * @xp_rate * @level
+        @max_hp = @max_hp * @xp_rate * @level
+        @max_mp = @max_mp * @xp_rate * @level
     end
     def to_s
         @nom
